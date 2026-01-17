e65570466c9ed3fa7e2c703821602c4117d3b830
Vincent <vinsch32@icloud.com>
2026-01-17 17:57:27 +0100

    yak cp script

---
+++ sysdeps/yak/cp.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp ~/osdev/yak/yak-src/kernel/include/yak-abi/syscall.h include/yak/syscall.h
+cp ~/osdev/yak/yak-src/kernel/arch/x86_64/include/yak/arch-syscall.h x86_64/include/yak/arch-syscall.h
